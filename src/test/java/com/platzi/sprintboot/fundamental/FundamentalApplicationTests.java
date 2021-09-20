81ca4f8f623ae5521dfaee3487d060b64cb68294
Eudis Duarte <rene.duarte@hotmail.com>
2021-09-20 17:26:28 -0500

    primer commit

---
+++ src/test/java/com/platzi/sprintboot/fundamental/FundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.sprintboot.fundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
